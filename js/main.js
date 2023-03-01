bf26cecd14287708e06b9d373c15e227de1bb900
Lauren MacIver <laurmac@Laurens-MacBook-Pro-5.local>
2023-02-28 20:25:48 -0500

    add brushing?

--- js/main.js
+++ js/main.js
@@ -6,18 +6,36 @@ const VIS_HEIGHT = F_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = F_WIDTH - MARGINS.left - MARGINS.right;
 
 // Creating color constant
-    const COLORS = d3.scaleOrdinal()
+const COLORS = d3.scaleOrdinal()
         .domain(["setosa", "versicolor", "virginica"])
         .range(["green", "orange", "purple"]);
 
-
-function vis1() {
-  // frame for column 1
-  const FRAME1 = d3.select("#column1")
+// frame for column 1
+const FRAME1 = d3.select("#column1")
             .append("svg")
               .attr("height", F_HEIGHT)
               .attr("width", F_WIDTH)
               .attr("class", "frame");
+
+// frame for column 2
+const FRAME2 = d3.select("#column2")
+            .append("svg")
+              .attr("height", F_HEIGHT)
+              .attr("width", F_WIDTH)
+              .attr("class", "frame");
+
+// frame for column 3
+const FRAME3 = d3.select("#column3")
+            .append("svg")
+              .attr("height", F_HEIGHT)
+              .attr("width", F_WIDTH)
+              .attr("class", "frame");
+
+let F1_points, F2_points; 
+let xVal, yVal;
+
+
+function vis1() {
 
   // read from scatter-data.csv
   d3.csv("data/iris.csv").then((data) => {
@@ -43,7 +61,8 @@ function vis1() {
 
 
     // Use X_SCALE and Y_SCALE to plot our points
-    FRAME1.selectAll("points")  
+    F1_points = FRAME1.append("g")
+        .selectAll("points")  
         .data(data) // passed from .then  
         .enter()       
         .append("circle")  
@@ -82,21 +101,15 @@ vis1()
 /*--------------------------------------------------------------*/
 
 function vis2() {
-  // frame for column 2
-  const FRAME2 = d3.select("#column2")
-            .append("svg")
-              .attr("height", F_HEIGHT)
-              .attr("width", F_WIDTH)
-              .attr("class", "frame");
 
   // read from scatter-data.csv
   d3.csv("data/iris.csv").then((data) => {
 
-    const X_SCALE = d3.scaleLinear()
+    const xVal = d3.scaleLinear()
               .domain([0, 5])
               .range([0, VIS_WIDTH])
 
-    const Y_SCALE = d3.scaleLinear()
+    const yVal = d3.scaleLinear()
               .domain([0,3])
               .range([VIS_HEIGHT,0])
 
@@ -107,48 +120,29 @@ function vis2() {
     // add x-axis to vis
     FRAME2.append("g")
             .attr("transform", "translate(" + MARGINS.left+ "," + (VIS_HEIGHT + MARGINS.top) + ")")
-            .call(d3.axisBottom(X_SCALE).ticks(10));
+            .call(d3.axisBottom(xVal).ticks(10));
 
     // add y-axis to vis
     FRAME2.append("g")
             .attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.top) + ")")
-            .call(d3.axisLeft(Y_SCALE).ticks(14));
+            .call(d3.axisLeft(yVal).ticks(14));
 
     // Use X_SCALE to plot our points
-    FRAME2.selectAll("points")  
+    F2_points = FRAME2.append('g')
+        .selectAll("points")  
         .data(data) // passed from .then  
         .enter()       
         .append("circle")  
-          .attr("cx", (d) => { return (X_SCALE(d.Sepal_Width) + MARGINS.left); }) 
-          .attr("cy", (d) => { return (Y_SCALE(d.Petal_Width) + MARGINS.top); }) 
+          .attr("cx", (d) => { return (xVal(d.Sepal_Width) + MARGINS.left); }) 
+          .attr("cy", (d) => { return (yVal(d.Petal_Width) + MARGINS.top); }) 
           .attr("r", 4)
           .attr("class", "point")
           .attr("id", (d) => { return ("(" + d.Sepal_Length + ", " + d.Petal_Length + ")"); })
           .style("fill", function(d) {return COLORS(d.Species); })
           .style("opacity", 0.5);
 
-    // highlight on mouseover
-    function handleMouseover(event, d) {
-      d3.select(this).style("opacity", ".7")
-        .style("stroke-width", "3")
-        .style("stroke", "red");
-    }
-
-     // unhighlight on mouseleave
-    function handleMouseleave(event, d) {
-      d3.select(this).style("opacity", ".5")
-        .style("stroke-width", "0");
-    }
-
-
-    // add event listeners to the frame
-    FRAME2.selectAll("circle")
-                      .on("mouseover", handleMouseover)
-                      .on("mouseleave", handleMouseleave);
-
-
-
   }) 
+
 }
 
 vis2()
@@ -156,12 +150,6 @@ vis2()
 /*--------------------------------------------------------------*/
 
 function vis3() {
-  // frame for column 3
-  const FRAME3 = d3.select("#column3")
-            .append("svg")
-              .attr("height", F_HEIGHT)
-              .attr("width", F_WIDTH)
-              .attr("class", "frame");
 
   // read from scatter-data.csv
   d3.csv("data/iris.csv").then((data) => {
@@ -205,3 +193,52 @@ function vis3() {
 }
 
 vis3()
+
+/*--------------------------------------------------------------*/
+
+function brushAndLink() {
+  if(d3.event.selection) {
+    F2_points.attr("stroke", "none");
+    var [[x0, y0],[x1,y1]] = d3.event.selection;
+
+    // T/F based on if point is in selection
+    x = xVal(d.Sepal_Width) + MARGINS.left;
+    y = yVal(d.Petal_Width) + MARGINS.top;
+    var isSelected = x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
+    F2_points.classed("brushed", function(d) {
+      return isSelected;
+    })
+    F1_points.classed("brushed", function(d) {
+      return isSelected;
+    })
+
+    
+  }
+}
+
+FRAME2.call(d3.brush()
+  .extent([[0,0], [F_WIDTH, F_HEIGHT]])
+  .on("start brush", brushAndLink));
+
+/*
+    // highlight on mouseover
+    function handleMouseover(event, d) {
+      d3.select(this).style("opacity", ".7")
+        .style("stroke-width", "3")
+        .style("stroke", "red");
+    }
+
+     // unhighlight on mouseleave
+    function handleMouseleave(event, d) {
+      d3.select(this).style("opacity", ".5")
+        .style("stroke-width", "0");
+    }
+
+
+    // add event listeners to the frame
+    FRAME2.selectAll("circle")
+                      .on("mouseover", handleMouseover)
+                      .on("mouseleave", handleMouseleave);
+*/
+
+
